403661427f6604e93e09f7519f5f3b6dd85055de
theovilardo <theovilardo@gmail.com>
2023-05-03 20:27:51 -0300

    agrego primer ejercico

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -34,10 +34,97 @@ usuarioDePublicacion (u, _, _) = u
 likesDePublicacion :: Publicacion -> [Usuario]
 likesDePublicacion (_, _, us) = us
 
+-- Ejemplos
+
+usuario1 = (1, "Juan")
+usuario2 = (2, "Natalia")
+usuario3 = (3, "Pedro")
+usuario4 = (4, "Mariela")
+usuario5 = (5, "Natalia")
+
+relacion1_2 = (usuario1, usuario2)
+relacion1_3 = (usuario1, usuario3)
+relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
+relacion2_3 = (usuario3, usuario2)
+relacion2_4 = (usuario2, usuario4)
+relacion3_4 = (usuario4, usuario3)
+
+publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
+publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
+publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
+publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
+publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+
+publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
+
+publicacion3_1 = (usuario3, "Lorem Ipsum", [])
+publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
+publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+
+publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
+publicacion4_2 = (usuario4, "I am Bob", [])
+publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
+
+
+usuariosA = [usuario1, usuario2, usuario3, usuario4]
+relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
+publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
+redA = (usuariosA, relacionesA, publicacionesA)
+
+usuariosB = [usuario1, usuario2, usuario3, usuario5]
+relacionesB = [relacion1_2, relacion2_3]
+publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
+redB = (usuariosB, relacionesB, publicacionesB)
+
 -- Ejercicios
 
+-- describir qué hace la función: devuelve los nombres de usuario de la red social
 nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios = undefined
+nombresDeUsuarios (a,b,c) = eliminaRepetidos (proyectarNombres (usuarios (a,b,c)))
+
+-- Funciones Auxiliares
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece a [] = False
+pertenece a (x:xs) | a == x = True
+                   | otherwise = pertenece a xs
+
+nombreUsuario2 :: [Usuario] -> [String]
+nombreUsuario2 (x:xs) | null (x:xs) = []
+                      | (x:xs) == [x] = [snd x]
+                      | otherwise = [snd (head (x:xs))]
+
+proyectarNombres :: [Usuario] -> [String]
+proyectarNombres (x:xs) | null (x:xs) = ["final"]
+                        | (x:xs) == [x] = nombreUsuario2 (x:xs)
+                        | otherwise = nombreUsuario2 (x:xs) ++ proyectarNombres xs
+
+
+todosDistintos :: Eq a => [a] -> Bool
+todosDistintos [] = True
+todosDistintos [x] = True
+todosDistintos (x:xs) | pertenece x xs = False
+                 | otherwise = todosDistintos xs
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar a (x:xs) | null (x:xs) = []
+                | not (pertenece a (x:xs)) = x:xs
+                | a == x = xs
+                | otherwise = quitar a xs
+
+quitNomY :: (Eq t) => [t] -> [t]
+quitNomY [] = []
+quitNomY (x:xs) | todosDistintos (x:xs) = x:xs                -- ignorar
+                | pertenece x xs = quitNomY (quitar x xs)
+                | otherwise = x : quitNomY xs
+
+eliminaRepetidos :: Eq a => [a] -> [a]
+eliminaRepetidos [] = []
+eliminaRepetidos (x:xs) = x : eliminaRepetidos (quita x xs)
+  where quita _ [] = []
+        quita y (z:zs) | y == z    = quita y zs
+                       | otherwise = z : quita y zs
 
 -- describir qué hace la función: .....
 amigosDe :: RedSocial -> Usuario -> [Usuario]
