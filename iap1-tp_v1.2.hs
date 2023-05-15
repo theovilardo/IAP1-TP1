e0fdfdfc9eba894df7683190b0ea389362ab1e06
Sofgl <sofiagerpel@gmail.com>
2023-05-15 16:43:47 -0300

    Update iap1-tp_v1.3.hs
    Corrijo la función cantidadDeAmigosAcu (ahora llamada tieneMasAmigos) porque daban mal algunos resultados.
    
    Cambié el nombre de algunas funciones para que sean más declarativos.
    
    También, agregué el comentario del ejercicio 10 y le hice una pequeña adición a uno que otro.

--- iap1-tp_v1.2.hs
+++ iap1-tp_v1.2.hs
@@ -38,7 +38,8 @@ usuarioDePublicacion (u, _, _) = u
 likesDePublicacion :: Publicacion -> [Usuario]
 likesDePublicacion (_, _, us) = us
 
--- Ejercicios
+-- EJERCICIOS
+
 
 -- Funciones auxiliares que se utilizan en distintos ejercicios
 
@@ -61,9 +62,17 @@ quitar a (x:xs) | not (pertenece a (x:xs)) = x:xs
                 | a == x = xs 
                 | otherwise = x : quitar a xs
 
--- describir qué hace la función: EJERCICIO 1 -> Devuelve los nombres de usuario de la red social
+compartenElementos :: (Eq t) => [t] -> [t] -> Bool
+compartenElementos [] _ = False
+compartenElementos _ [] = False
+compartenElementos (x:xs) ys | pertenece x ys && (x:xs) == [x] = True
+                             | not(pertenece x ys) = False
+                             | otherwise = compartenElementos xs ys 
+
+
+-- EJERCICIO 1 -> Devuelve un conjunto con los nombres de usuario de la red social
 nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios (a,b,c) = eliminaRepetidos (proyectarNombres (usuarios (a,b,c)))
+nombresDeUsuarios (us,rs,ps) = eliminaRepetidos (proyectarNombres (usuarios (us,rs,ps)))
 
 -- Funciones Auxiliares
 
@@ -71,53 +80,62 @@ proyectarNombres :: [Usuario] -> [String]
 proyectarNombres [] = []
 proyectarNombres (x:xs) = nombreDeUsuario x : proyectarNombres xs
 
--- describir qué hace la función: EJERCICIO 2 -> Devuelve la lista de amigos que tiene el usuario ingresado (dentro de la red)
---Le falta función para eliminar los repetidos
+
+-- EJERCICIO 2 -> Devuelve un conjunto con los amigos que tiene el usuario ingresado dentro de la red
 amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe rd u = listaDeAmigos u (relaciones rd)
+amigosDe (us,rs,ps) u = eliminaRepetidos (listaDeAmigosDe u (relaciones (us,rs,ps)))
+
+-- Funciones Auxiliares
+
+listaDeAmigosDe :: Usuario -> [Relacion] -> [Usuario]
+listaDeAmigosDe u [] = []
+listaDeAmigosDe u (x:xs) | u == fst x = snd x : listaDeAmigosDe u xs
+                         | u == snd x = fst x : listaDeAmigosDe u xs
+                         | otherwise = listaDeAmigosDe u xs 
 
 
-listaDeAmigos :: Usuario -> [Relacion] -> [Usuario]
-listaDeAmigos u [] = []
-listaDeAmigos u (x:xs) | u == fst x = snd x : listaDeAmigos u xs
-                       | u == snd x = fst x : listaDeAmigos u xs
-                       | otherwise = listaDeAmigos u xs 
-
--- describir qué hace la función: EJERCICIO 3 -> Devuelve la cantidad de amigos que tiene el usuario ingresado (dentro de la red)
+-- EJERCICIO 3 -> Devuelve la cantidad de amigos que tiene el usuario ingresado dentro de la red
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos (us,rs,ps) u = longitud (amigosDe (us,rs,ps) u)
 
--- describir qué hace la función: EJERCICIO 4 -> Dada la red social, devuelve el usuario con mas amigos
+
+-- EJERCICIO 4 -> Devuelve el usuario con más amigos dentro de la red social
+-- Si dos o más usuarios tienen la mayor cantidad de amigos devuelve cualquiera de ellos
+-- Si la mayor cantidad de amigos es 0 devuelve cualquier usuario de la red
 usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos (us,rs,ps) = cantidadDeAcu (us,rs,ps) (usuarios (us,rs,ps))
+usuarioConMasAmigos (us,rs,ps) = tieneMasAmigos (us,rs,ps) (usuarios (us,rs,ps))
 
 -- Funciones Auxiliares
-cantidadDeAcu :: RedSocial -> [Usuario] -> Usuario
-cantidadDeAcu (us,[],ps) (x:xs) = x
-cantidadDeAcu rs (x:xs) | null (x:xs) = x
-                        | cantidadDeAmigos (rs) x > cantidadDeAmigos (rs) (head xs) = x
-                        | otherwise = cantidadDeAcu rs xs
 
--- describir qué hace la función: EJERCICIO 5 -> Si dentro de la red hay un usuario con mas de un millon de amigos devuelve True, sino False
+tieneMasAmigos :: RedSocial -> [Usuario] -> Usuario
+tieneMasAmigos (us,[],ps) (x:xs) = x
+tieneMasAmigos (us,rs,ps) (x:xs) | xs == [] = x
+                                 | cantidadDeAmigos (us,rs,ps) x > cantidadDeAmigos (us,rs,ps) (head xs) = tieneMasAmigos (us,rs,ps) (x : (tail xs))
+                                 | otherwise = tieneMasAmigos (us,rs,ps) xs
+
+
+-- EJERCICIO 5 -> Devuelve True si dentro de la red hay un usuario con más de un diez amigos
 estaRobertoCarlos :: RedSocial -> Bool
 estaRobertoCarlos (us,rs,ps) | cantidadDeAmigos (us,rs,ps) (usuarioConMasAmigos (us,rs,ps)) > 10 = True
                              | otherwise = False
 
--- describir qué hace la función: EJERCICIO 6 -> devuelve las publicaciones de un usuario dentro de la red social
+
+-- EJERCICIO 6 -> Devuelve un conjunto con las publicaciones de un usuario dentro de la red social
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
-publicacionesDe (us,rs,ps) u = pubDe (publicaciones (us,rs,ps)) u
+publicacionesDe (us,rs,ps) u = eliminaRepetidos (listaDePublicacionesDe (publicaciones (us,rs,ps)) u)
 
 -- Funciones Auxiliares
 
-pubDe :: [Publicacion] -> Usuario -> [Publicacion]
-pubDe [] _ = []
-pubDe (x:xs) u | null (x:xs) = [x]
-               | usuarioDePublicacion x == u = x : pubDe xs u
-               | otherwise = pubDe xs u
+listaDePublicacionesDe :: [Publicacion] -> Usuario -> [Publicacion]
+listaDePublicacionesDe [] _ = []
+listaDePublicacionesDe (x:xs) u | null (x:xs) = [x]
+                              | usuarioDePublicacion x == u = x : listaDePublicacionesDe xs u
+                              | otherwise = listaDePublicacionesDe xs u
 
--- describir qué hace la función: EJERCICIO 7 -> Devuelve las publicaciones que le gustaron al usuario ingresado (dentro de la red)
+
+-- EJERCICIO 7 -> Devuelve un conjunto con las publicaciones que le gustaron al usuario ingresado (dentro de la red)
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA (us,rs,ps) u = eliminaRepetidos (tusLikes (publicaciones (us,rs,ps)) u) -- FALTA PROBAR LOS CASOS DE REPETICION
+publicacionesQueLeGustanA (us,rs,ps) u = eliminaRepetidos (tusLikes (publicaciones (us,rs,ps)) u)
 
 -- Funciones Auxiliares
 
@@ -127,63 +145,48 @@ tusLikes (x:xs) u | null (x:xs) = [x]
                   | pertenece u (likesDePublicacion x) = x : tusLikes xs u
                   | otherwise = tusLikes xs u
 
--- describir qué hace la función: EJERCICIO 8 -> Devuelve True si los usuarios ingresados le dieron me gusta a las mismas publicaciones #Actualizado para la version 2.1 del TP
+
+-- EJERCICIO 8 -> Devuelve True si los usuarios ingresados le dieron me gusta a las mismas publicaciones #Actualizado para la version 2.1 del TP
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
-lesGustanLasMismasPublicaciones (us,rs,ps) a b | longitud (publicacionesQueLeGustanA (us,rs,ps) a) == longitud (publicacionesQueLeGustanA (us,rs,ps) b) && mismosElementos (publicacionesQueLeGustanA (us,rs,ps) a) (publicacionesQueLeGustanA (us,rs,ps) b) = True
-                                               | otherwise = False
+lesGustanLasMismasPublicaciones (us,rs,ps) u1 u2 | longitud (publicacionesQueLeGustanA (us,rs,ps) u1) == longitud (publicacionesQueLeGustanA (us,rs,ps) u2) && compartenElementos (publicacionesQueLeGustanA (us,rs,ps) u1) (publicacionesQueLeGustanA (us,rs,ps) u2) = True
+                                                 | otherwise = False
 
--- Funciones Auxiliares
 
-mismosElementos :: (Eq t) => [t] -> [t] -> Bool
-mismosElementos [] _ = False
-mismosElementos _ [] = False
-mismosElementos (x:xs) ys | pertenece x ys && (x:xs) == [x] = True
-                          | not(pertenece x ys) = False
-                          | otherwise = mismosElementos xs ys 
-
--- describir qué hace la función: EJERCICIO 9 -> Devuelve True si existe un usuario que le haya dado like a todas las publicaciones hechas por el usuario ingresado
+-- EJERCICIO 9 -> Devuelve True si existe un usuario que le haya dado like a todas las publicaciones hechas por el usuario ingresado
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel (us,rs,ps) u = likesPertenecen (us,rs,ps) (quitar u (usuarios (us,rs,ps))) (publicacionesDe (us,rs,ps) u)
+tieneUnSeguidorFiel (us,rs,ps) u = algunoDioLikeATodas (us,rs,ps) (quitar u (usuarios (us,rs,ps))) (publicacionesDe (us,rs,ps) u)
 
 -- Funciones Auxiliares
 
-likesPertenecen :: RedSocial -> [Usuario] -> [Publicacion] -> Bool
-likesPertenecen (_,_,_) [] _ = False
-likesPertenecen (us,rs,ps) _ [] = False
-likesPertenecen (us,rs,ps) (x:xs) (y:ys) | perteneceLike (y:ys) (publicacionesQueLeGustanA (us,rs,ps) x) = True
-                                         | otherwise = likesPertenecen (us,rs,ps) xs (y:ys)
+algunoDioLikeATodas :: RedSocial -> [Usuario] -> [Publicacion] -> Bool
+algunoDioLikeATodas (_,_,_) [] _ = False
+algunoDioLikeATodas (us,rs,ps) _ [] = False
+algunoDioLikeATodas (us,rs,ps) (x:xs) (y:ys) | compartenElementos (y:ys) (publicacionesQueLeGustanA (us,rs,ps) x) = True
+                                             | otherwise = algunoDioLikeATodas (us,rs,ps) xs (y:ys)
 
-perteneceLike :: (Eq t) => [t] -> [t] -> Bool
-perteneceLike [] [] = False
-perteneceLike _ [] = False
-perteneceLike (y:ys) (x:xs) | (y:ys) == [y] && pertenece y (x:xs) = True
-perteneceLike (y:ys) (x:xs) | pertenece y (x:xs) = perteneceLike ys (x:xs)
-                            | otherwise = False
-
--- describir qué hace la función: EJERCICIO 10 -> Devuelve true si en la red existe una cadena de amistades
--- Explicacion:
--- La idea es crear una lista aparte que contiene a todos los amigos del u1 y a los amigos de sus amigos y a los amigos de los amigos de sus amigos...
--- Entonces, si u2 esta relacionado con el u1 a través de alguna de sus amistades, u2 tendría que aparecer en esa lista.
-
+ 
+-- EJERCICIO 10 -> Devuelve True si en la red existe una cadena de amistades
+-- Para que exista una cadena de amistades se tiene que llegar de uno al otro a través de relaciones directas o indirectas
+-- Relación directa: el usuario es amigo del usuario ingresado
+-- Relación indirecta: el usuario no es amigo del usuario ingresado, pero tiene amigos en común o sus amigos tienen amigos en común con el usuario ingresado
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos rs u1 u2 | (amigosDe rs u1) == [] || (amigosDe rs u2) == [] = False
-                                 | pertenece u1 (amigosDe rs u2) = True
-                                 | pertenece u2 (listaDeAmigos2 rs u1 (amigosDe rs u1)) = True
-                                 | otherwise = False
-                                 
+existeSecuenciaDeAmigos (us,rs,ps) u1 u2 | (amigosDe (us,rs,ps) u1) == [] || (amigosDe (us,rs,ps) u2) == [] = False
+                                         | pertenece u1 (amigosDe (us,rs,ps) u2) = True
+                                         | pertenece u2 (amistadDirectaEIndirecta (us,rs,ps) u1 (amigosDe (us,rs,ps) u1)) = True
+                                         | otherwise = False   
+                              
+-- Crea una lista que contiene todos los usuarios con los que se relaciona directa o indirectamente el usuario ingresado. 
+amistadDirectaEIndirecta :: RedSocial -> Usuario -> [Usuario] -> [Usuario]
+amistadDirectaEIndirecta (us,rs,ps) u1 [] = []
+amistadDirectaEIndirecta (us,rs,ps) u1 (x:xs) = [x] ++ (amigosDeAmigos (us,rs,ps) (amigosDe (us,rs,ps) x) (u1:x:[])) ++ amistadDirectaEIndirecta (us,rs,ps) u1 xs 
 
-listaDeAmigos2 :: RedSocial -> Usuario -> [Usuario] -> [Usuario]
-listaDeAmigos2 rs u1 [] = []
-listaDeAmigos2 rs u1 (x:xs) = [x] ++ (amigosDeAmigos rs (amigosDe rs x) (u1:x:[])) ++ listaDeAmigos2 rs u1 xs 
-
--- amigosDeAmigos recibe 2 listas. La primera son los amigos de la cabeza de la lista que aparece en la función anterior.
--- La segunda es una lista aparte que contiene a todos los usuarios a los que yo ya me fijé quienes eran sus amigos, esto es para evitar un bucle infinito.
--- La función quitar del where es para quitar a todos los elementos de esa lista de la lista de amigos del usuario
-
+--Crea una lista con los amigos de los amigos de los usuarios y en cada recursión quita de la primera lista los usuarios que ya fueron chequeados para evitar un bucle infinito
 amigosDeAmigos :: RedSocial -> [Usuario] -> [Usuario] -> [Usuario]
-amigosDeAmigos rs [] ys = []
-amigosDeAmigos rs (x:xs) ys = [x] ++ amigosDeAmigos (us,rs,ps) (quitarLista ys (amigosDe rs x)) (x:ys) ++ amigosDeAmigos (us,rs,ps) xs ys
+amigosDeAmigos (us,rs,ps) [] ys = []
+amigosDeAmigos (us,rs,ps) (x:xs) ys = [x] ++ (amigosDeAmigos (us,rs,ps) (quitarLista ys (amigosDe (us,rs,ps) x)) (x:ys)) ++ amigosDeAmigos (us,rs,ps) xs ys
  where quitarLista (y:ys) [] = []
        quitarLista [] (z:zs) = (z:zs)
        quitarLista (y:ys) (z:zs) | (y:ys) == [y] = quitar y (z:zs)
                                  | otherwise = quitarLista ys (quitar y (z:zs))
+
+
